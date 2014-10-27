aad65c02886a7bebb2a5fe6c5b6febe462bdc460
mkutmon <mkutmon@gmail.com>
2014-10-27 15:52:47 -0700

    updated list organism file

--- new-webservice/list-organisms.py
+++ new-webservice/list-organisms.py
@@ -3,16 +3,20 @@
 # author: msk (mkutmon@gmail.com)
 ###
 
-import requests
+import httplib2
 from lxml import etree as ET
 
 # define namespaces
 namespaces = {'ns1':'http://www.wso2.org/php/xs','ns2':'http://www.wikipathways.org/webservice'}
 
 # list organisms
-r = requests.get('http://test2.wikipathways.org/wpi/webservicetest/?method=listOrganisms&format=xml')
-dom = ET.fromstring(r.text)
+h = httplib2.Http()
+(resp_headers, content) = h.request("http://test2.wikipathways.org/wpi/webservicetest/webservice.php/listOrganisms", "GET")
+
+dom = ET.fromstring(content)
 
 # print each organism
 for node in dom:
 	print node.text
+
+
