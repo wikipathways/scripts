06f700a93dfad8ed500e44c639a3571a44f581d5
Martina Summer-Kutmon <mkutmon@gmail.com>
2015-02-27 20:46:30 +0100

    added example for update pathway and fixed create pathway

--- new-webservice/update-pathway.py
+++ new-webservice/update-pathway.py
@@ -12,7 +12,12 @@ from lxml import etree as ET
 
 username = 'Mkutmon'
 gpml_file = 'test.gpml'
+wp_id = "WP2892"
 basis_url = 'http://pvjs.wikipathways.org/wpi/webservicetest/'
+description = 'test update webservice function'
+
+# update revision!!
+revision = "78850"
 
 ##################################
 
@@ -33,7 +38,6 @@ for node in dom.findall('ns1:auth', namespaces):
 f = open(gpml_file, 'r')
 gpml = f.read()
 
-# create pathway
-update_params = {'gpml': gpml, 'auth' : authentication, 'username': username}
-re = requests.post(basis_url + 'createPathway', params=update_params)
-print re.text
+# update pathway
+update_params = {'pwId': wp_id, 'description' : description, 'gpml' : gpml, 'revision' : revision, 'auth' : authentication, 'username': username}
+requests.post(basis_url + 'updatePathway', params=update_params)
